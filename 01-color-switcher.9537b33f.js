067e4eb0970876dcba3e21bfb8dc4da4571c75ca
Viacheslav-Malyi <99422703+Viacheslav-Malyi@users.noreply.github.com>
2022-08-31 17:49:41 +0000

    Deploying to gh-pages from @ Viacheslav-Malyi/goit-js-hw-09@7f5ebe5ee909d370d9320b1e256a3136c513fac1 🚀

--- 01-color-switcher.9537b33f.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){if(r)return;r=!0,o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){r=!1,clearInterval(o)}));var r=!1,o=null}();
-//# sourceMappingURL=01-color-switcher.9537b33f.js.map
